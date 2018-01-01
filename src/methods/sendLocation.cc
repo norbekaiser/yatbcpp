d6175968b03c3accf520d0faab285a93942f0be8
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-01-01 02:19:56 +0100

    [CI] Merge branch 'pbuilder-start' into 'master'
    PBUILDER integration und div send funktionen
    
    See merge request yatbcpp/yatbcpp!1

--- src/methods/sendLocation.cc
+++ src/methods/sendLocation.cc
@@ -17,7 +17,7 @@
 #include <experimental/optional> 
 #define optional experimental::optional 
 #endif 
-#include "methods/sendVoice.h"
+#include "methods/sendLocation.h"
 
 using namespace std;
 using namespace yatbcpp;
@@ -26,42 +26,39 @@ using namespace yatbcpp;
 // Constructor Section                                                                                                //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-sendVoice::sendVoice(Chat C, std::string voice) :
-        telegram_methodJSON("sendVoice"), telegram_methodMultipart("sendVoice"),
-        chat_id(to_string(C.getId())), voice(voice)
+sendLocation::sendLocation(Chat C,Location location) :
+        telegram_methodJSON("sendLocation"),
+        chat_id(to_string(C.getId())), latitude(location.getLatitude()), longitude(location.getLongitude())
 {
-
+    
 }
 
-sendVoice::sendVoice(int chat_id, std::string voice) :
-        telegram_methodJSON("sendVoice"), telegram_methodMultipart("sendVoice"),
-        chat_id(to_string(chat_id)), voice(voice)
+sendLocation::sendLocation(int chat_id,Location location) :
+        telegram_methodJSON("sendLocation"),
+        chat_id(to_string(chat_id)), latitude(location.getLatitude()), longitude(location.getLongitude())
 {
-
+    
 }
 
-sendVoice::sendVoice(string chat_id, std::string voice) :
-        telegram_methodJSON("sendVoice"), telegram_methodMultipart("sendVoice"),
-        chat_id(chat_id), voice(voice)
+sendLocation::sendLocation(string chat_id,Location location) :
+        telegram_methodJSON("sendLocation"),
+        chat_id(chat_id), latitude(location.getLatitude()), longitude(location.getLongitude())
 {
-
+    
 }
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Outgoing Section                                                                                                   //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-Json::Value sendVoice::toJson() {
+Json::Value sendLocation::toJson() {
     Json::Value Outgoing;
     Outgoing["chat_id"] = getChat_id();
-    Outgoing["voice"] = getVoice();
+    Outgoing["latitude"] = getLatitude();
+    Outgoing["longitude"] = getLongitude();
 
-    if(getCaption()){
-        Outgoing["caption"] = getCaption().value();
-    }
-    if(getDuration()){
-        Outgoing["duration"] = getDuration().value();
+    if(getLivePeriod()){
+        Outgoing["live_period"] = getLivePeriod().value();
     }
     if(getDisable_notification()){
         Outgoing["disable_notification"] = getDisable_notification().value();
@@ -74,96 +71,77 @@ Json::Value sendVoice::toJson() {
 
 }
 
-void sendVoice::add_to_post(struct curl_httppost **start, struct curl_httppost **end) {
-    curl_formadd(start,end,
-                 CURLFORM_COPYNAME,"chat_id",
-                 CURLFORM_COPYCONTENTS,chat_id.c_str(),
-                 CURLFORM_END);
-    curl_formadd(start,end,
-                 CURLFORM_COPYNAME,"voice",
-                 CURLFORM_FILE,voice.c_str(),
-                 CURLFORM_END);
-    if(caption){
-        curl_formadd(start,end,
-                     CURLFORM_COPYNAME,"caption",
-                     CURLFORM_COPYCONTENTS,caption.value().c_str(),
-                     CURLFORM_END);
-    }
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Setter Section                                                                                                     //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void sendVoice::setCaption(const optional<string> &caption) {
-    sendVoice::caption = caption;
+
+void sendLocation::setLive_period(const optional<unsigned int> &live_period) {
+    sendLocation::live_period = live_period;
 }
 
-
-void sendVoice::setDuration(const optional<unsigned int> &duration) {
-    sendVoice::duration = duration;
+void sendLocation::setDisable_notification(const std::optional<bool> &disable_notification) {
+    sendLocation::disable_notification = disable_notification;
 }
 
-void sendVoice::setDisable_notification(const std::optional<bool> &disable_notification) {
-    sendVoice::disable_notification = disable_notification;
+void sendLocation::setReply_to_message_id(const std::optional<int> &reply_to_message_id) {
+    sendLocation::reply_to_message_id = reply_to_message_id;
 }
 
-void sendVoice::setReply_to_message_id(const std::optional<int> &reply_to_message_id) {
-    sendVoice::reply_to_message_id = reply_to_message_id;
-}
 
 /**
  * Sets an ReplyKeyboardMarkup
  * @param RKM
  */
-void sendVoice::setReplyMarkup(const ReplyKeyboardMarkup RKM){
-    sendVoice::reply_markup = RKM;
+void sendLocation::setReplyMarkup(const ReplyKeyboardMarkup RKM){
+    sendLocation::reply_markup = RKM;
 }
 
 /**
  * Sets an ReplyKeyboardRemove
  * @param RKR
  */
-void sendVoice::setReplyMarkup(const ReplyKeyboardRemove RKR){
-    sendVoice::reply_markup = RKR;
+void sendLocation::setReplyMarkup(const ReplyKeyboardRemove RKR){
+    sendLocation::reply_markup = RKR;
 }
 
 /**
  * Sets an ForceReply
  * @param FR
  */
-void sendVoice::setReplyMarkup(const ForceReply FR){
-    sendVoice::reply_markup = FR;
+void sendLocation::setReplyMarkup(const ForceReply FR){
+    sendLocation::reply_markup = FR;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Getter Section                                                                                                     //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const string &sendVoice::getChat_id() const {
+const string &sendLocation::getChat_id() const {
     return chat_id;
 }
 
-const string &sendVoice::getVoice() const {
-    return voice;
+const float &sendLocation::getLatitude() const {
+    return latitude;
 }
 
-const optional<string> &sendVoice::getCaption() const {
-    return caption;
+const float &sendLocation::getLongitude() const {
+    return longitude;
 }
 
-const optional<unsigned int> &sendVoice::getDuration() const {
-    return duration;
+const optional<unsigned int> &sendLocation::getLivePeriod() const {
+    return live_period;
 }
 
-const optional<bool> &sendVoice::getDisable_notification() const {
+const optional<bool> &sendLocation::getDisable_notification() const {
     return disable_notification;
 }
 
-const optional<int> &sendVoice::getReply_to_message_id() const {
+const optional<int> &sendLocation::getReply_to_message_id() const {
     return reply_to_message_id;
 }
 
-const optional<ReplyMarkup> &sendVoice::getReply_markup() const {
+const optional<ReplyMarkup> &sendLocation::getReply_markup() const {
     return reply_markup;
 }
